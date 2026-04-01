94c3cf0d223c8400e262bb5a8bf9f8973d8352e5
Hasancan <hasofreeman@gmail.com>
2026-04-01 15:44:00 +0300

    Update matrix_lib.py

--- src/matrix_lib.py
+++ src/matrix_lib.py
@@ -1,12 +1,26 @@
 import numpy as np
-from scipy.sparse import lil_matrix
+from scipy.sparse import coo_matrix
 
 class SparseStiffnessMatrix:
     def __init__(self, size):
         self.size = size
-        # LIL formatı kurulum (assemble) sırasında hızlıdır
-        self.matrix = lil_matrix((size, size))
+        # Verileri biriktirmek için listeler kullanıyoruz
+        self.row_indices = []
+        self.col_indices = []
+        self.data_values = []
 
     def assemble(self, row, col, value):
-        # Python 1-tabanlı indexten 0-tabanlıya geçiş
-        self.matrix[row-1, col-1] += value
+        """Veriyi listeye ekler (LIL'den çok daha hızlıdır)."""
+        if value != 0:
+            self.row_indices.append(row - 1)
+            self.col_indices.append(col - 1)
+            self.data_values.append(value)
+
+    def finalize(self):
+        """Listelerdeki veriyi tek seferde Sparse Matrix'e dönüştürür."""
+        print(f"Birim matrisler birleştiriliyor (Toplam {len(self.data_values)} giriş)...")
+        K = coo_matrix(
+            (self.data_values, (self.row_indices, self.col_indices)),
+            shape=(self.size, self.size)
+        )
+        return K.tocsr() # Hesaplama için CSR formatına çevir
